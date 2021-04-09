0462f1a9ff4a594639fb4e6c03d886d6ea3de72b
BHUSHAN <raj.bhushan@capgemini.com>
2021-04-09 14:48:39 +0530

    web-app module file added

--- powerboard-backend/src/app/app.module.ts
+++ powerboard-backend/src/app/app.module.ts
@@ -2,22 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
-import { TeamsModule } from './web-app/teams/teams.module';
-import { BusinessUnitsModule } from './web-app/business-units/business-units.module';
-import { ClientStatusModule } from './web-app/client-status/client-status.module';
-import { SprintModule } from './web-app/sprint/sprint.module';
-import { TeamSpiritModule } from './web-app/team-spirit/team-spirit.module';
-import { CodeQualitySnapshotModule } from './web-app/code-quality-snapshot/code-quality-snapshot.module';
+import { ElectronAppModule } from './electron-app/electron-app.module';
+import { WebAppModule } from './web-app/web-app.module';
 
 @Module({
   imports: [
     CoreModule,
-    TeamsModule,
-    BusinessUnitsModule,
-    ClientStatusModule,
-    CodeQualitySnapshotModule,
-    SprintModule,
-    TeamSpiritModule,
+    WebAppModule,
+    ElectronAppModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ powerboard-backend/src/app/web-app/web-app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TeamsModule } from './teams/teams.module';
+import { BusinessUnitsModule } from './business-units/business-units.module';
+import { ClientStatusModule } from './client-status/client-status.module';
+import { SprintModule } from './sprint/sprint.module';
+import { TeamSpiritModule } from './team-spirit/team-spirit.module';
+import { CodeQualitySnapshotModule } from './code-quality-snapshot/code-quality-snapshot.module';
+
+@Module({
+  imports: [
+    TeamsModule,
+    BusinessUnitsModule,
+    ClientStatusModule,
+    CodeQualitySnapshotModule,
+    SprintModule,
+    TeamSpiritModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class WebAppModule {}
